d500cae2e35fa9c7b174b50a9e05f73baee5fafe
kundannitt <kundankumarnitt@gmail.com>
2023-03-01 09:53:10 +0530

    update

---
+++ Lab1/input1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // this is another comment
+    printf("Hello World!");
+
+    /* this is comment*/
+
+    return 0;
+}
---
+++ Lab2/input1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    // this is another comment
+    printf("Hello World!");
+
+    /* this is comment*/
+    int x=5;
+    if (x==4)
+    {
+    	printf("4\n");
+    }
+    else if 
+    {
+    	printf("maybe 4\n");
+    else
+    {
+    	printf("invalid\n");
+    }
+    
+    return 0;
+}
